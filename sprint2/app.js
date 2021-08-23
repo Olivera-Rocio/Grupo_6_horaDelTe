bef11905ac3ab322cd9a42dd597e7a979b0bca50
olivera rocio <layoyi2015@gmail.com>
2021-08-23 09:48:11 -0300

    agrega rutas a app.js

--- sprint2/app.js
+++ sprint2/app.js
@@ -5,6 +5,10 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views','index.html')));
+app.get('/productDetail', (req, res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views','login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views','register.html')));
+app.get('/productCart', (req, res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto' + port))
