d59db3f4a5139707c389b2e86a551e9e3e5f3a01
Nati <medaigualqueseafacil@gmail.com>
2021-10-01 22:02:37 -0300

    requiere elementos necesarios en el controlador

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 let products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'),'utf-8'));
-
+let categories = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'categories.json'),'utf-8'));
 
 const toDiscount = require('../utils/toDiscount');
 
