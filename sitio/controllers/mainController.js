8d96c4d334d1166a1d70fbf2c7f81f16d2f04874
olivera rocio <layoyi2015@gmail.com>
2021-10-01 23:21:54 -0300

    modifica precio-descuento

--- sitio/controllers/mainController.js
+++ sitio/controllers/mainController.js
@@ -1,9 +1,18 @@
-const products = require('../data/products.json')
+const fs = require('fs');
+const path = require('path');
+
+const products= require("../data/products.json");
+//let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
+
+const toThousand = require("../utils/toThousand");
+const toDiscount = require("../utils/toDiscount");
 
 module.exports = {
     index : (req, res) => {
 return res.render('index',{
-    products
+    products,
+    toThousand,
+    toDiscount
 })
     },
      admin : (req,res) => {
