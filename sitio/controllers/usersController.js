ceaae0779e9165d40f2b0d2968d0031f329fe97d
olivera rocio <layoyi2015@gmail.com>
2021-10-13 20:10:39 -0300

    implementa registro-usuarios

--- sitio/controllers/usersController.js
+++ sitio/controllers/usersController.js
@@ -1,6 +1,27 @@
+const bcrypt = require('bcryptjs');
+const fs = require('fs');
+const path = require('path');
+let users = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'users.json'), 'utf-8'));
+
 module.exports = {
     register : (req,res) => {
         return res.render("register")
+    },
+    processRegister: (req, res) => {
+        //return res.send(req.body)
+            const { name, email, telefono, password } = req.body;
+            let user = {
+                id: users[users.length - 1].id + 1,
+                name: name.trim(),
+                email: email.trim(),
+                telefono: telefono.trim(),
+                password: bcrypt.hashSync(password, 10),
+                avatar: "default.png",
+                rol: "user"
+            }
+            users.push(user);
+            fs.writeFileSync(path.join(__dirname, '../data/users.json'), JSON.stringify(users, null, 3), 'utf-8');
+            return res.redirect('/')
     },
     login : (req,res) => {
         return res.render("login")
--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
  
-const {register, login} = require('../controllers/usersController')
+const {register,processRegister,login} = require('../controllers/usersController')
 
  /* users */
 router.get('/register',register);
+router.post('/register',processRegister);
 router.get('/login',login);
 
 module.exports = router;
