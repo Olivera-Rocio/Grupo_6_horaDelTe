203db6b27952111091a4809a159524260395d744
olivera rocio <layoyi2015@gmail.com>
2021-10-22 13:42:19 -0300

    aplica multer y valida productAdd

--- sitio/controllers/productsController.js
+++ sitio/controllers/productsController.js
@@ -5,10 +5,10 @@ const productFilePath = path.join(__dirname, '../data/products.json');
 
 let products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json'),'utf-8'));
 
-
 const toDiscount = require('../utils/toDiscount');
 const checkId = require('../utils/checkId');
 const toThousand = require('../utils/toThousand') ; 
+const {validationResult} = require('express-validator');
 
 module.exports = {
     index: (req, res) => {
@@ -51,60 +51,61 @@ module.exports = {
     
         res.redirect('/admin')
     
-         },
+    },
     create: (req,res) => {
-return res.render('productAdd')
+        return res.render('productAdd')
+    },
+    store: (req, res) => {
+        let errors = validationResult(req);
 
-} ,
-
-store: (req, res) => {
+        if(errors.isEmpty()){
+            const {
+                name,price,discount,description,category
+            } = req.body;
+            let product = {
+                id: (products[products.length-1].id + 1),
+                name, 
+                price : +price, 
+                discount : +discount, 
+                category, 
+                description, 
+                image : req.file ? req.file.filename :'default-image.png'
+            }
+            products.push(product);
+            fs.writeFileSync(productFilePath,JSON.stringify(products, null, 2),'utf-8');
+            
+            return res.redirect ('/admin')
+        }else{
+            return res.render('productAdd',{
+                errors : errors.mapped(),
+                old : req.body
+            })
+        }
+    },
+    edit : (req,res) => {
+        return res.render('productModify',{
+            product : products.find(product => product.id === +req.params.id),
+            toThousand,
+        })
+    },
+    update: (req,res) => {
+        const { name, price, discount, category, description} = req.body;
     
-    const {
-        name,price,discount,description,category
-    } = req.body;
-    let product = {
-        id: (products[products.length-1].id + 1),
-        name, 
-        price : +price, 
-        discount : +discount, 
-        category, 
-        description, 
-        image : 'default-image.png'
+        let product =products.find(product => product.id === +req.params.id);
+    
+        let productEdited = {
+            id : +req.params.id,
+            name: name.trim(),
+            price: +price,
+            discount: +discount,
+            category,
+            description: description.trim(),
+            image: req.file ? req.file.filename : product.image
+        };
+    
+        let productsEdited = products.map(product => product.id === +req.params.id ? productEdited : product)
+    
+        fs.writeFileSync(path.join(__dirname,'..', 'data', 'products.json'), JSON.stringify(productsEdited,null,3), 'utf-8');
+        res.redirect('/admin') 
     }
-
- 
-products.push(product);
-fs.writeFileSync(productFilePath,JSON.stringify(products, null, 2),'utf-8');
-
-return res.redirect ('/')
-}
-
-,
-edit : (req,res) => {
-        
-    return res.render('productModify',{
-        product : products.find(product => product.id === +req.params.id),
-        toThousand,
-    })
-},
-
-update: (req,res) => {
-    const { name, price, discount, category, description} = req.body;
-
-    let product =products.find(product => product.id === +req.params.id);
-
-    let productEdited = {
-        id : +req.params.id,
-        name: name.trim(),
-        price: +price,
-        discount: +discount,
-        category,
-        description: description.trim(),
-        image: req.file ? req.file.filename : product.image
-    };
-
-    let productsEdited = products.map(product => product.id === +req.params.id ? productEdited : product)
-
-    fs.writeFileSync(path.join(__dirname,'..', 'data', 'products.json'), JSON.stringify(productsEdited,null,3), 'utf-8');
-    res.redirect('/admin') 
-} } 
+ } 
---
+++ sitio/validations/productValidator.js
@@ -0,0 +1,26 @@
+const {check} = require('express-validator');
+
+module.exports = [
+    check('name')
+    .notEmpty().withMessage('Nombre de producto obligatorio'),
+
+    check('description')
+    .notEmpty().withMessage('Descripción obligatoria').bail()
+    .isLength({
+        min : 20
+    }).withMessage('Descripción debe tener un mímino de 20 caracteres'),
+
+    check('category')
+    .notEmpty().withMessage('Indicar categoría'),
+    
+    check('price')
+    .isInt({
+        min: 1
+    }).withMessage('Ingresar un número válido'),
+
+    check('discount')
+    .isInt({
+        min: 0
+    }).withMessage('Ingresar un número válido')
+    
+]
