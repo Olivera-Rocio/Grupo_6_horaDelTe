fb076e674409984ac85b54e103492be3e019772b
olivera rocio <layoyi2015@gmail.com>
2021-12-17 19:04:21 -0300

    arregla admin y su responsive

--- sitio/src/database/models/category.js
+++ sitio/src/database/models/category.js
@@ -12,8 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Category.hasMany(models.Product,{
-        as: "products",
-        foreignKey : "categoryId"
+        as: "products"
       })
     }
   };
