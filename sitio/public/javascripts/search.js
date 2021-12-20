79a2962283116387f01aa9168ad563ff5298c1c2
Nati <medaigualqueseafacil@gmail.com>
2021-12-20 12:29:24 -0300

    modificaion de las validaciones back y front de perfil

--- sitio/public/javascripts/search.js
+++ sitio/public/javascripts/search.js
@@ -1,12 +1,11 @@
 console.log('header, header-admin sucess');
 
-const $ = id => document.getElementById(id);
-const formSearch = $('form-search');
+const formSearch = document.getElementById('form-search');
 
  formSearch.addEventListener('submit', function (e) {
     
     e.preventDefault();
-    var inputSearch = $("input-search")
+    var inputSearch = document.getElementById("input-search")
     console.log(inputSearch.value.trim().length);
     
     if (inputSearch.value.trim().length != 0) {
--- sitio/src/validations/profileValidator.js
+++ sitio/src/validations/profileValidator.js
@@ -1,12 +1,42 @@
 const { check, body } = require('express-validator');
 
+const regExPhone = /^[0-9]/
+
 module.exports = [
 
     check('name')
         .notEmpty().withMessage('El nombre es requerido'),
+    body('name')
+        .custom(value => {
+            let nombreSeparado = value.split(" ");
+
+            let nombre = nombreSeparado[0];
+
+            let apellido = nombreSeparado[1];
+            
+            
+                if (nombre.trim().length < 2 || apellido.trim().length < 2) {
+                    return false
+                } else {
+                    return true
+                }
+
+        }).withMessage('El nombre y el apellido debe tener 2 o más caracteres'),
 
     check('telefono')
         .notEmpty().withMessage('El número de teléfono es requerido'),
+
+        body('telefono')
+        .custom(value => {
+            
+            
+                if (!regExPhone.test(value)) {
+                    return false
+                } else {
+                    return true
+                }
+
+        }).withMessage('Ingresa un numero valido'),
 
     body('password')
         .custom(value => {
